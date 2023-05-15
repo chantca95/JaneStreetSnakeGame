ae19e09635cf42bbb9be22e79e1f6328f8a0c6ee
Alex Chan <chantca95@meta.com>
2023-05-14 17:00:24 -0700

    Initial Commit

---
+++ JaneStreetSnakeGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  JaneStreetSnakeGame
+//
+//  Created by Alex Chan on 5/14/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
